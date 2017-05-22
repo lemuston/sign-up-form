8bcec5e7389443bdcc949ca40569dcd43e0c0dcb
Laura Mustonen <laura.mustonen@cs.helsinki.fi>
2017-05-22 12:52:16 +0300

    correction

--- feis.php
+++ feis.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "rooneyfi_2016fg";
-$password = "{%P![[BHngpp";
-$dbname = "rooneyfi_inspiralfeisgrades";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
